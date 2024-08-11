7c9c0d1920cd4a8fbbc13d99ba9b3e3c3b5085fd
Oleksander Ostapenko <polygrafsoft@gmail.com>
2024-08-11 15:40:58 +0300

    add interface

---
+++ ManagerRDP/RDPConnection.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagerRDP
+{
+    internal class RDPConnection
+    {
+        public string URL { get; set; }
+        public int Port { get; set; }
+
+        public string User { get; set; }
+        public string Password { get; set; }
+
+        public bool UseVPN { get; set; }
+    }
+}
